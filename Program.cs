d4273b3bd3ad315f8d36eb7b2e8f042ef1fccad6
Omarf2000 <98235663+Omarf2000@users.noreply.github.com>
2022-12-16 15:38:16 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//عمر محمد عبد القتاح حسن عماره 
+            //مجموعه 2
+            //سكشن 14
+            Console.Write("please enter First Num : ");
+            int FirstNum = int.Parse(Console.ReadLine());
+            Console.Write("please enter second Num : ");
+            int secondNum = int.Parse(Console.ReadLine());
+
+
+            bool Prime = true;
+            Console.WriteLine("Prime Numbers between " + FirstNum + " to " + secondNum + " :");
+            for (int a = FirstNum; a <= secondNum; a++) 
+            {
+                for (int b = 2; b <= 100; b++)
+                {
+                    if (a != b && a % b == 0) 
+                    {
+                        Prime = false;
+
+                        break;
+                    }
+                }
+                if (Prime)
+                {
+                    Console.Write("\t" + a); 
+                }
+                Prime = true;
+            }
+
+
+
+            Console.ReadKey(); 
+        }
+    }
+}
